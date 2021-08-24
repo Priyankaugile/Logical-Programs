26352634860bcf5d9c28e4ed2ab7a71240ac3e05
priyankaugile <ugilepriyanka26@gmail.com>
2021-08-25 00:11:48 +0530

    Added: code of stop watch.

--- logicalprograms.java
+++ logicalprograms.java
@@ -1,30 +1,52 @@
 import java.util.Scanner;
 
-public class CouponNumber {
+public class StopWatch {
+
 
     public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter how many coupon number you want: ");
-        int num = sc.nextInt();
-        char [] chars="abcdefghijklmnopqrstuvwxyz@#$%&*0123456789".toCharArray();
-        int max = 100000000;
-       int randome = (int)(Math.random()*max);
-        StringBuffer n = new StringBuffer();
+        System.out.println("Press '1' to Start Time: ");
+        Scanner scan = new Scanner(System.in);
+        int timer = scan.nextInt();
+        stopWatch.start();
 
-        while(randome>0){
-            for (int i=0; i<=num; i++) {
+        System.out.println();
+        System.out.println("Press '2' to Stop Time: ");
 
-                 System.out.println("number:" +chars.length);
-                 n.append(chars[randome% chars.length]);
-                 randome /= chars.length;
+        int timer1 = scan.nextInt();
 
-            }
-        }
+        stopWatch.stop();
 
-        String couponCode = n.toString();
-        System.out.println("coupon code " +couponCode);
-        return;
+        long l = stopWatch.getElapsedTime();
+        System.out.println();
+        System.out.println("Total Time Elapsed(in millisec) is:" + l);
+        System.out.println();
+        System.out.println("Converting millisec to seconds: " + (l / 1000) + " sec");
+
     }
+
+    public long startTimer = 0;
+    public long stopTimer = 0;
+    public long elapsed;
+
+    //to start timer
+    public void start() {
+        startTimer = System.currentTimeMillis();
+        System.out.println("Start Time is: " + startTimer);
+    }
+
+    // to stop timer
+    public void stop() {
+        stopTimer = System.currentTimeMillis();
+        System.out.println("Stop Time is: " + stopTimer);
+    }
+
+    public long getElapsedTime() {
+        elapsed = stopTimer - startTimer;
+        return elapsed;
+    }
+
 }
+
 
